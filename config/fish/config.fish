5f68887872d7e1c59ebfcccae053d8c25d0fccd8
Masahiro Nagae <two.big.suns@gmail.com>
2016-11-29 17:59:13 +0900

    Add enviroment variables

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,3 +8,8 @@ set -x PATH $PATH $HOME/.nodebrew/current/bin
 function fish_user_key_bindings
     bind \cr 'peco_select_history (commandline -b)'
 end
+
+# Typetalk
+if test -f ~/Dropbox/typetalk.fish
+    source ~/Dropbox/typetalk.fish
+end
