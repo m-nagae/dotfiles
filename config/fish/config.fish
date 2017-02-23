d476db197c39f19a261f91312edce95d9366d546
Masahiro Nagae <two.big.suns@gmail.com>
2017-02-23 14:23:48 +0900

    Add config for anyenv

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,14 +1,15 @@
-# nodebrew
-if not test -d ~/.nodebrew/src
-    mkdir -p ~/.nodebrew/src
-end
-set -x PATH $PATH $HOME/.nodebrew/current/bin
+set -x EDITOR vim
 
 # peco
 function fish_user_key_bindings
     bind \cr 'peco_select_history (commandline -b)'
 end
 
+# anyenv
+if test -d ~/.anyenv/bin
+    set -x PATH $HOME/.anyenv/bin $PATH
+    status --is-interactive; and source (anyenv init -|psub)
+end
+
 # direnv
 eval (direnv hook fish)
-set -x EDITOR vim
