1a0462559fcb3b475047b29d732e09af1bd5f287
Masahiro Nagae <two.big.suns@gmail.com>
2017-06-06 12:26:26 +0900

    Change anyenv to asdf

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,8 +5,5 @@ function fish_user_key_bindings
     bind \cr 'peco_select_history (commandline -b)'
 end
 
-# anyenv
-if test -d ~/.anyenv/bin
-    set -x PATH $HOME/.anyenv/bin $PATH
-    status --is-interactive; and source (anyenv init -|psub)
-end
+# asdf
+source /usr/local/opt/asdf/asdf.fish
